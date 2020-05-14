ed6ba74c02897ac4696676c3045db45e24e26d12
Ariane F <Ariane@Ingrid.local>
2020-05-14 17:45:41 -0400

    add interviewer name to Application component render

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,32 +3,49 @@ import axios from 'axios';
 
 import DayList from 'components/DayList.js';
 import Appointment from 'components/Appointment'
-import getAppointmentsForDay from '../helpers/selectors';
+import { getAppointmentsForDay, getInterview } from '../helpers/selectors';
 
 import "components/Application.scss";
+
+// a feature of this architecture: keep persistent stuff all in the same level up above
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const setDay = day => setState({ ...state, day });
 
+  // this useEffect only executes once AFTER render of all components and updates the virtual DOM accordingly.
   useEffect(() => {
     Promise.all([
       Promise.resolve(axios.get('/api/days')),
-      Promise.resolve(axios.get('/api/appointments'))
+      Promise.resolve(axios.get('/api/appointments')),
+      Promise.resolve(axios.get('/api/interviewers'))
     ])
     .then(all => {
-      setState((prev) => ({...prev, days: all[0].data, appointments: all[1].data}))
+      setState((prev) => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
     })
   }, [])
+  // console.log(state.interviewers)
 
   const appointments = getAppointmentsForDay(state, state.day);
+
+
   const appointmentList = appointments.map( appointment => {
-    return <Appointment key={appointment.id} {...appointment}/>
+    const interview = getInterview(state, appointment.interview);
+    
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
   })
   
   return (
