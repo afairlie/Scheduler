d726231ff60b7548cf23d9327336cd0bd850583e
Ariane F <Ariane@Ingrid.local>
2020-05-16 21:00:40 -0400

    minor style changes

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -38,6 +38,7 @@ const getInterview = (state, interview) => {
 const findDayByAppt = (state, id) => {
   const filteredDay = state.days.filter((day) => {
     return day.appointments.includes(id) && day;
+    
   })[0]
 
   const dayCopy = {...filteredDay, appointments: [...filteredDay.appointments], interviewers: [...filteredDay.interviewers]}
